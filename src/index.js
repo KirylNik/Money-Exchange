5e3916ec5691d014ddf2f3d6b3f6b4635c1c5679
KirylNik <ultramylo@gmail.com>
2018-02-21 14:59:48 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    let result = {};
+
+    if (currency <= 0) {
+        return (result)
+    } else if (currency >= 10000) {
+        return ({error: "You are rich, my friend! We don't have so much coins for exchange"})
+    } else {
+
+        let remainder = 0;
+        //Получение колличества 50-центовых монет. {"H":1,"Q":1,"D":1,"N":1,"P":1}
+        if (currency >= 50) {
+            result["H"] = Math.floor(currency / 50);
+            remainder = currency % 50;
+        } else {
+            remainder = currency;
+        };
+        //Получение колличества 25-центовых монет.
+        if (remainder >= 25) {
+            result["Q"] = Math.floor(remainder / 25);
+            remainder = currency % 25;
+        };
+        //Получение колличества 10-центовых монет.
+        if (remainder >= 10) {
+            result["D"] = Math.floor(remainder / 10);
+            remainder = currency % 10;
+        };
+        //Получение колличества 5-центовых монет.
+        if (remainder >= 5) {
+            result["N"] = Math.floor(remainder / 5);
+            remainder = currency % 5;
+        };
+        //Получение колличества 5-центовых монет.
+        if (remainder >= 1) {
+            result["P"] = Math.floor(remainder / 1);
+        };
+
+        return (result);
+    }
+};
